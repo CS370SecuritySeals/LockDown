4b28c84da4cd48d2c9279b35c1a1bad5c17257d2
Marguerite Williams <33711594+willimarmei@users.noreply.github.com>
2018-04-01 20:37:03 -0700

    Fixed minor issues and was able to get a successful build. Set up view framework for the change password view Set up view framework for google maps

---
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ChangePasswordScreen.java
@@ -0,0 +1,29 @@
+package com.sp18.ssu370.baseprojectapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ChangePasswordScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
