01bb3d2a9aa1b1b6e124d788502a54cb88218b21
Marguerite Williams <33711594+willimarmei@users.noreply.github.com>
2018-04-02 10:37:30 -0700

    Worked on button implementation to change view from main activity to the change passcode view

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -1,17 +1,34 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.sp18.ssu370.baseprojectapp.ChangePasswordScreen;
 import com.sp18.ssu370.baseprojectapp.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button changePCButton;
+    private TextView PCButtonText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changePCButton = (Button)findViewById(R.id.change_pc_button);
+
+        changePCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivities(new Intent(MainActivity.this, ChangePasswordScreen.class));
+            }
+        });
     }
-    private Button changePCButton;
+
+    private void startActivities(Intent intent) {
+    }
+
 }
