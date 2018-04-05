e9359ee08125d73ac717035482e69b2072523bf8
Marguerite Williams <33711594+willimarmei@users.noreply.github.com>
2018-04-04 18:28:22 -0700

    Added button restriction to phone view

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -1,14 +1,19 @@
 package com.sp18.ssu370.baseprojectapp.ui.activities;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sp18.ssu370.baseprojectapp.ChangePasswordScreen;
-import com.sp18.ssu370.baseprojectapp.ExitLockdownActivity;
 import com.sp18.ssu370.baseprojectapp.MapsActivity;
 import com.sp18.ssu370.baseprojectapp.PhoneActivity;
 import com.sp18.ssu370.baseprojectapp.R;
@@ -50,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, PhoneActivity.class));
             }
         });
+
 
     }
 }
