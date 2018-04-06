0d88104266e6bbb13c1489eb38d4c68ac7dd1be6
Miss Swenson <CSready6>
2018-04-05 18:26:44 -0700

    progress on change passcode

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ChangePasswordScreen.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ChangePasswordScreen.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.sp18.ssu370.baseprojectapp.ui.activities.MainActivity;
+import sqlite.DatabaseHelper;
 
 public class ChangePasswordScreen extends AppCompatActivity {
     private Button returnHomeButton;
     private Button tempSettingsButton;
+    private Button changePCButton;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,16 @@ public class ChangePasswordScreen extends AppCompatActivity {
         setContentView(R.layout.activity_change_password_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        db = new DatabaseHelper(this);
+
+        changePCButton = (Button) findViewById(R.id.change_pc_button);
+        changePCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChangePasswordScreen.this, UpdatePasscodeActivity.class));
+            }
+        });
 
         returnHomeButton = (Button) findViewById(R.id.return_home);
         returnHomeButton.setOnClickListener(new View.OnClickListener() {
