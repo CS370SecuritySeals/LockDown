e309de808d9a14bc1a5501ff4f8f73e4d935936d
swensonk <36430826+swensonk@users.noreply.github.com>
2018-04-16 00:40:12 -0700

    implemented exit lockdown to accept and process passcode to return to the home screen (will need question implementation later)

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/PhoneActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/PhoneActivity.java
@@ -1,29 +1,16 @@
 package com.sp18.ssu370.baseprojectapp;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.renderscript.ScriptGroup;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.widget.EditText;
 import android.text.TextUtils;
 import android.net.Uri;
-
-
-
-import com.sp18.ssu370.baseprojectapp.ui.activities.MainActivity;
 
 public class PhoneActivity extends AppCompatActivity {
     private Button exitLockDownButton;
