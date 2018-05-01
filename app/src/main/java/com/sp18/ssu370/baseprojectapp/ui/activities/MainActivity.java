b5b25e32cb0cc894820596706de20d94d0d3cb7b
Marguerite Williams <33711594+willimarmei@users.noreply.github.com>
2018-04-30 17:53:14 -0700

    Added search and current location functionality for the map activity

--- app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/sp18/ssu370/baseprojectapp/ui/activities/MainActivity.java
@@ -12,6 +12,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Dialog;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import com.sp18.ssu370.baseprojectapp.ChangePasswordScreen;
 import com.sp18.ssu370.baseprojectapp.MapsActivity;
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView code;
     private DatabaseHelper db;
     final Context context = this;
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     public static void setOnTouchListener(OnSwipeTouchListener onTouchListener) {
         MainActivity.onTouchListener = onTouchListener;
@@ -135,4 +142,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 }
